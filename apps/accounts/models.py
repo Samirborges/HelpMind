4b627f5ea4d31c654b5347e634c52ee8d12bfc5b
Samirborges <alessandro.samir@souunit.com.br>
2026-05-02 16:43:52 -0300

    feat(accounts): reset database and apply custom user migrations

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ["email", "full_name"]
+
+    def __str__(self):
+        return self.email
--- helpmind/settings.py
+++ helpmind/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts',
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+AUTH_USER_MODEL = "accounts.User"
 
 
 # Internationalization
